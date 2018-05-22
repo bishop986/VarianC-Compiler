28339b03669ce5f9dc4b68e24612e8eabdf18e45
bishop986 <774925471@qq.com>
2018-05-22 10:05:00 +0800

    词法分析，添加了注释测试用例和浮点数测试用例

--- TestSample/test1.c
+++ TestSample/test1.c
@@ -2,12 +2,17 @@ int f() {
 	return 200;
 }
 
+/*
+ * SSSSji
+ */
+// int g = 1;
 int i = 1;
-float f1 = .21f;
+float f1 = .21;
 float f2 = 242.;
-float f3 = 2.3f;
+float f3 = 2.3;
 float f4 = 2E+2;
 float f5 = 2.3;
+ 
 
 int main() {    
 	int main;
---
+++ TestSample/test2.c
@@ -0,0 +1,4 @@
+float f1 = 2.;
+float f2 = 3.32;
+float f3 = 2.f;
+float f4 = .2;
